cc6202cdfe9a85a00e765c6074b3d30c5e3a7849
Quentin Hoareau <hoareauquentin97480@gmail.com>
2020-10-19 00:47:16 +0400

    Amélioration / Changements "Erreur"
    - Appel au Css pour la vue erreur
    - Amélioration du style pour la vue erreur
    - Changement de la vue erreur

--- vue/vueErreur.php
+++ vue/vueErreur.php
@@ -1,4 +1,6 @@
-<div class="erreur" style="font-size: 27px;">
-    <p>Nous sommes désolés, il y a eu un problème lors du chargement de la page demandée.</p>
+<div class="erreur" style="">
+    <h1>BHL Clothes</h1>
+    <h3>Page non trouvée</h3>
+    <p>La page demandée n'a pas été trouvée sur notre serveur.</p>
     <a href="accueil"> <input type="submit" value="Retour à l'accueil"></a>
 </div>
